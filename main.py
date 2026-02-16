aba1fcf39fd75f3bcfd5998ccb6ea726ed3f6ca5
DaDuDBen <ezhilnilavan7405@gmail.com>
2026-02-16 22:24:09 +0530

    added User Independence

--- main.py
+++ main.py
@@ -41,8 +41,10 @@ def create_task(
 ):
     db_task = Task(
         title=task.title,
-        description=task.description
+        description=task.description,
+        user_id=current_user.id
     )
+
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
@@ -54,9 +56,11 @@ def get_tasks(
     db: Session = Depends(get_db),
     limit: int = Query(10, ge=1, le=100),
     offset: int = Query(0, ge=0),
-    title: str | None = None
+    title: str | None = None,
+    current_user: User = Depends(get_current_user)
 ):
-    query = db.query(Task)
+    query = db.query(Task).filter(Task.user_id == current_user.id)
+
 
     if title:
         query = query.filter(Task.title.ilike(f"%{title}%"))
@@ -79,16 +83,21 @@ def get_tasks(
 
 
 @app.get("/tasks/{task_id}", response_model=TaskResponse)
-def get_task(task_id: int, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == task_id).first()
+def get_task(task_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    task = db.query(Task).filter(
+    Task.id == task_id,
+    Task.user_id == current_user.id).first()
+
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
 
 @app.put("/tasks/{task_id}", response_model=TaskResponse)
-def update_task(task_id: int, updated: TaskCreate, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == task_id).first()
+def update_task(task_id: int, updated: TaskCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    task = db.query(Task).filter(
+    Task.id == task_id,
+    Task.user_id == current_user.id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
 
@@ -101,8 +110,10 @@ def update_task(task_id: int, updated: TaskCreate, db: Session = Depends(get_db)
     
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int, db: Session = Depends(get_db)):
-    task = db.query(Task).filter(Task.id == task_id).first()
+def delete_task(task_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    task = db.query(Task).filter(
+    Task.id == task_id,
+    Task.user_id == current_user.id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found")
 
--- models.py
+++ models.py
@@ -1,12 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
 
 class Task(Base):
     __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
+    title = Column(String)
+    description = Column(String)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User")
 
 class User(Base):
     __tablename__ = "users"
